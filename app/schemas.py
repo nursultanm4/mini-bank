99cdc4d2a97facab3cc46de0d9dbfef27420fe8e
NursultanMyrza <nursultanmyrza12@gmail.com>
2025-07-24 15:53:07 -1200

    Implement password necessary policies

--- app/schemas.py
+++ app/schemas.py
@@ -5,6 +5,23 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
+    @validator('password')
+    def password_policy(cls, v):
+
+        if len(v) < 8:
+            raise ValueError('Password must be at least 8 characters long')
+        
+        if not any(char.isdigit() for char in v):
+            raise ValueError('Password must contain at least one digit')
+
+        if not any(char.isalpha() for char in v):
+            raise ValueError('Password must contain at least one letter')
+        
+        if not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>?/" for char in v):
+            raise ValueError('Password must contain at least one special character')
+        
+        return v
+
 
 class Token(BaseModel):
     access_token: str
@@ -19,6 +36,13 @@ class AccountCreate(BaseModel):
     def name_must_not_be_blank(cls, v):
         if not v.strip():
             raise ValueError('Name must not be blank')
+        
+        # Remove unnecessary / dangerous chars
+        unnecessary = ['<', '>', '"', "'", ';', '--']
+        for char in unnecessary:
+            if char in v:
+                raise ValueError('Account name contains invalid characters')
+
         return v
 
     
