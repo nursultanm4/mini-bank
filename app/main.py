5335567f85e405c64e3826c305ee21b33e86e8f7
NursultanMyrza <nursultanmyrza12@gmail.com>
2025-07-24 17:03:22 -1200

    Refactor codebase

--- app/main.py
+++ app/main.py
@@ -10,6 +10,17 @@ from slowapi.util import get_remote_address
 from fastapi import Request
 from slowapi.errors import RateLimitExceeded
 from slowapi.extension import Limiter as LimiterExtension
+import os
+import logging
+
+
+os.makedirs("logs", exist_ok=True)
+# Configure logging
+logging.basicConfig(
+    filename="logs/app.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
 
 
 # Create limiter instance
@@ -53,6 +64,7 @@ async def register(user: UserCreate, request: Request):
         # Check if user already exists
         existing = session.exec(select(User).where(User.username == user.username)).first() 
         if existing:
+            logging.warning(f"Attempt to regiser existing username: {user.username} from {request.client.host}")
             raise HTTPException(status_code=400, detail="Username already registered")
         hashed_pw = hash_password(user.password)
         # CRUD ( Create, Read, Update, Delete )
@@ -60,6 +72,7 @@ async def register(user: UserCreate, request: Request):
         session.add(db_user)
         session.commit()
         session.refresh(db_user)
+        logging.info(f"New user registered: {user.username} from {request.client.host}")
         return db_user
 
 
@@ -70,14 +83,16 @@ async def login(user: UserCreate, request: Request):
     with Session(engine) as session:
         db_user = session.exec(select(User).where(User.username == user.username)).first()
         if not db_user:
+            logging.warning(f"Failed login atttempt for username: {user.username} from {request.client.host}")
             raise HTTPException(status_code=401, detail="Invalid credentials")
+        logging.info(f"Successful login for username: {user.username} from {request.client.host}")
         access_token = create_access_token(data={"sub": db_user.username})
         return Token(access_token=access_token, token_type="bearer")
 
 
 # Create a new account
 @app.post("/accounts/", response_model=Account)
-def create_account(
+async def create_account(
     account: AccountCreate,
     current_user: User = Depends(get_current_user)
 ):
@@ -95,17 +110,18 @@ def create_account(
 
 # Get account balance
 @app.get("/accounts/{account_id}/balance")
-def get_balance(account_id: int, current_user: User = Depends(get_current_user)):
+async def get_balance(account_id: int, current_user: User = Depends(get_current_user)):
     with Session(engine) as session:
         account = session.get(Account, account_id)
         if not account:
+            logging.warning(f"Account not found: {account_id} for user {current_user.username}")
             raise HTTPException(status_code=404, detail="Account not found")
         return {"account_id": account_id, "balance": account.balance}
 
 
 # Make a transaction
 @app.post("/transactions/", response_model=Transaction)
-def make_transaction(
+async def make_transaction(
     transaction: TransactionCreate,
     current_user: User = Depends(get_current_user)
     ):
@@ -113,9 +129,14 @@ def make_transaction(
         from_acc = session.get(Account, transaction.from_account_id)
         to_acc = session.get(Account, transaction.to_account_id)
         if not from_acc or not to_acc:
+            logging.warning(f"Transaction failed: invalid account(s) by user {current_user.username}")
             raise HTTPException(status_code=404, detail="Account not found")
         if from_acc.balance < transaction.amount:
+            logging.warning(f"Transaction failed: Insufficient funds by user {current_user.username}")
             raise HTTPException(status_code=400, detail="Insufficient funds")
+        if from_acc.user_id != current_user.id:
+            logging.warning(f"Unauthorized transaction attempt by user {current_user.username} on account {from_acc.id}")
+            raise HTTPException(status_code=403, detail="You can only send money from your own account")
         from_acc.balance -= transaction.amount
         to_acc.balance += transaction.amount
         db_transaction = Transaction(
@@ -127,12 +148,13 @@ def make_transaction(
         session.add(db_transaction)
         session.commit()
         session.refresh(db_transaction)
+        logging.info(f"Transaction: {transaction.amount} from {from_acc.id} to {to_acc.id} by user {current_user.username}")
         return db_transaction
 
 
 # List of all transactions
 @app.get("/transactions/")
-def get_transactions(current_user: User = Depends(get_current_user)):
+async def get_transactions(current_user: User = Depends(get_current_user)):
     with Session(engine) as session:
         # Get all accounts owned by the user
         user_accounts = session.exec(select(Account.id).where(Account.user_id==current_user.id)).first()
